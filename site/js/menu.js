892b2b305a8cd6fd0f1781c6357873f93276770d
itggot-Hugo-Engström-Svanberg <hugo.engstromsvanberg@itggot.se>
2018-05-25 12:39:22 +0200

    Added css and scss and javascript and pages.

--- site/js/menu.js
+++ site/js/menu.js
@@ -56,3 +56,12 @@ if (!event.target.matches('.dropbtn')) {
 }
 }
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
